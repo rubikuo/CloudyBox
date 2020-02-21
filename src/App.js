dce08d8ddd5338cea9ac4a8d0952910af056bef9
Rubi Kuo <rubitiara@gmail.com>
2020-02-21 11:18:24 +0100

    get token to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LogIn from "./LogIn/LogIn";
+
+import { Dropbox } from "dropbox";
 
 function App() {
+
+  // to access to the app
+  const dbx = new Dropbox({clientId: "bc9lq9cup2tcfps"});
+  // to get token by using getAuthenticationUrl 
+  // we will get location.hash 
+  // use queryString to parsel the location.hash to get the token
+  const url = dbx.getAuthenticationUrl("http://localhost:3000/auth");
+
   return (
     <div className="App">
-        <h1>GROUP PROJECT</h1>
-        <p>Hello</p>
+      <LogIn />
+      <a href={url}>Connect</a>
     </div>
   );
 }
---
+++ src/LogIn/LogIn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LogIn = () => {
+  return (
+    <>
+      <h1>Log In</h1>
+
+      <div className="">Image</div>
+
+      <button>Log In</button>
+    </>
+  );
+};
+
+export default LogIn;
