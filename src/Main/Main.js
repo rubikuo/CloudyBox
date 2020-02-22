f3d824027588f947db5e78ea8c392550a6689bc4
Rubi Kuo <rubitiara@gmail.com>
2020-02-23 00:08:00 +0100

    setup initial main component and fileList component

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,8 +1,31 @@
 import React from "react";
+import FileList from "../FileList/FileList";
+import "./Main.css";
+import { FaLanguage, FaStar } from "react-icons/fa";
 
-const Main = () =>{
+const Main = () => {
+  return (
+    <main>
+      <div className="titleBar">
+          <div className="tabsCtn">
+        <div className="tabs">
+          <p>Name</p>
+          <FaLanguage />
+        </div>
+        <div className="tabs">
+          <p>Stared</p>
+          <FaStar />
+        </div>
+        </div>
+        <p>MetaData</p>
+        <p>Modified</p>
+      </div>
+      <ul>
+        {/* map out the FileLsit, now just example*/}
+        <FileList />
+      </ul>
+    </main>
+  );
+};
 
-
-}
-
-export default Main
+export default Main;
