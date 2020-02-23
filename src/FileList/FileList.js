03a83bb4a1f5353f9552dfd836087eccaa0b5f37
Rubi Kuo <rubitiara@gmail.com>
2020-02-23 20:18:44 +0100

    added tab effect on titleBar

--- src/FileList/FileList.js
+++ src/FileList/FileList.js
@@ -5,9 +5,12 @@ import "./FileList.css";
 const FileList = () => {
   return (
     <li className="item">
+      <div className="itemSmlCtn">
       <FaStar className="starIcon" />
       <FaFolder className="folderIcon" />
+  
       <p>My new folder</p>
+      </div>
       <p>200 KB</p>
       <p>2020-02-22 10:55</p>
       <FaTrash />
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import FileList from "../FileList/FileList";
 import "./Main.css";
 import { FaLanguage, FaStar } from "react-icons/fa";
 
 const Main = () => {
+  const [tab, updateTab] = useState("name");
+  const showTab = tabName => {
+    updateTab(tabName);
+  };
+
+  let tabActiveStyle = {
+    backgroundColor: "rgb(235, 235, 235)",
+    color: "rgb(34, 138, 208)"
+  };
+
   return (
     <main>
       <div className="titleBar">
-          <div className="tabsCtn">
-        <div className="tabs">
-          <p>Name</p>
-          <FaLanguage />
+        <div className="tabsCtn">
+          <div
+            className="tabs"
+            style={tab === "name" ? tabActiveStyle : {}}
+            onClick={() => showTab("name")}
+          >
+            <p>Name</p>
+            <FaLanguage />
+          </div>
+          <div
+            className="tabs"
+            style={tab === "stared" ? tabActiveStyle : {}}
+            onClick={() => showTab("stared")}
+          >
+            <p>Stared</p>
+            <FaStar />
+          </div>
         </div>
-        <div className="tabs">
-          <p>Stared</p>
-          <FaStar />
+        <div className="tagCtn">
+          <p className="metaTag">MetaData</p>
+          <p className="modifiedTag">Modified</p>
         </div>
-        </div>
-        <p>MetaData</p>
-        <p>Modified</p>
       </div>
       <ul>
         {/* map out the FileLsit, now just example*/}
