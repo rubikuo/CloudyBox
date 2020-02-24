1ac1e531216554262941745d152e7ba9651af5b8
Devina <devinardya@gmail.com>
2020-02-24 10:59:09 +0100

    create sidebars

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,6 +3,7 @@ import { Dropbox } from "dropbox";
 import { token$, updateToken } from "../store";
 import Main from "../Main/Main";
 import "./Home.css";
+import Sidebar from "../Sidebar/Sidebar";
 
 const Home = ({ location }) => {
   const [localToken, updateLocalToken] = useState(token$.value);
@@ -22,13 +23,13 @@ const Home = ({ location }) => {
             </div>
             <div className = "content">
                 <div className ="sidebar menu">
-
+                    <Sidebar name = "sidebarMenu"/>
                 </div>
                 <div className ="mainArea">
                     <Main />
                 </div>
                  <div className ="sidebar buttons">
-
+                     <Sidebar name = "sidebarButtons"/>
                  </div>
             </div>  
             <div className = "footer">
