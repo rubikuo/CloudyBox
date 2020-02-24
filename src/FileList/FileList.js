1cee30f970b7e2a6fd55de951400fe14a814daba
Rubi Kuo <rubitiara@gmail.com>
2020-02-24 23:30:41 +0100

    files got rendered out in list

--- src/FileList/FileList.js
+++ src/FileList/FileList.js
@@ -2,17 +2,29 @@ import React from "react";
 import { FaFolder, FaStar, FaTrash } from "react-icons/fa";
 import "./FileList.css";
 
-const FileList = ({doc}) => {
+const FileList = ({ doc }) => {
+  const convertDate =(isoDate)=> {
+    if(isoDate){
+    var timeStr = isoDate;
+    var date = new Date(timeStr);
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var dateStr = month + "/" + day + "/" + year;
+    return dateStr;
+  }
+  }
+
   return (
     <li className="item">
       <div className="itemSmlCtn">
-      <FaStar className="starIcon" />
-      <FaFolder className="folderIcon" />
-  
-      <p>{doc.name}</p>
+        <FaStar className="starIcon" />
+        <FaFolder className="folderIcon" />
+
+        <p>{doc.name}</p>
       </div>
       <p>200 KB</p>
-      <p>{doc.client_modified}</p>
+      <p>{convertDate(doc.client_modified)}</p>
       <FaTrash />
     </li>
   );
