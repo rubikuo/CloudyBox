cf44f66633d855b0383b8a06964076c63810cfbf
Rubi Kuo <rubitiara@gmail.com>
2020-02-25 20:56:11 +0100

    accept the changes

--- src/FileList/FileList.js
+++ src/FileList/FileList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaFolder, FaStar, FaTrash } from "react-icons/fa";
+import { FaFolder, FaStar} from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
 import "./FileList.css";
 import { convertDate } from "./convertDate.js";
 import {convertBytes} from "./convertBytes.js";
@@ -17,7 +18,7 @@ const FileList = ({ doc }) => {
       </div>
       <p>{doc[".tag"]=== "file"? convertBytes(doc.size): "--"}</p>
       <p>{convertDate(doc.client_modified)}</p>
-      <FaTrash />
+      <button> <MdMenu /> </button>
     </li>
   );
 };
