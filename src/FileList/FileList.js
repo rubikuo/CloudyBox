bb1a23efc7a76a4c3ed7c5987f342b606801e16a
Rubi Kuo <rubitiara@gmail.com>
2020-02-26 15:42:46 +0100

    fixed dropDown toggle

--- src/FileList/FileList.js
+++ src/FileList/FileList.js
@@ -6,39 +6,66 @@ import { convertDate } from "./convertDate.js";
 import { convertBytes } from "./convertBytes.js";
 import { Link } from "react-router-dom";
 
-const FileList = ({ doc, updateModalType, updateModals, updateItemId, updateItemName }) => {
-   const activateModal =(name, id)=>{
-     updateModals(true);
-     updateModalType("remove");
-     updateItemName(name);
-     updateItemId(id);
-   }
+const FileList = ({
+  doc,
+  updateModalType,
+  updateModals,
+  updateItemId,
+  updateItemName
+}) => {
+  const [dropDown, updateDropDown] = useState(false);
 
-  if(doc){
-  console.log(doc[".tag"]);
+  const showDropDown = e => {
+    console.log(e.target.id);
+    updateDropDown(dropDown? false:true);
+  };
+  const activateModal = (name, id) => {
+    updateModals(true);
+    updateModalType("remove");
+    updateItemName(name);
+    updateItemId(id);
+  };
 
+  let dropdownClass;
+  if(dropDown){
+    dropdownClass = "dropDown active";
+  } else {
+    dropdownClass = "dropDown"
+  }
 
-  return (
-    <li className="item">
-      <div className="itemSmlCtn">
-        <FaStar className="starIcon" />
-        <FaFolder className="folderIcon" />
-        <Link className="documentLink" to="/">
-          {doc.name}
-        </Link>
-      </div>
-      <p className="metaData">{doc[".tag"] === "file" ? convertBytes(doc.size) : "--"}</p>
-      <p className="modified">{convertDate(doc.client_modified)}</p>
-      <button
-        onClick={() => {
-         activateModal(doc.name, doc.id)
-        }}
-      >
-        <FaTrash />
-      </button>
-    </li>
-  );
-      }
+  if (doc) {
+    return (
+      <li className="item">
+        <div className="itemSmlCtn">
+          <FaStar className="starIcon" />
+          <FaFolder className="folderIcon" />
+          <Link className="documentLink" to="/">
+            {doc.name}
+          </Link>
+        </div>
+        <p className="metaData">
+          {doc[".tag"] === "file" ? convertBytes(doc.size) : "--"}
+        </p>
+        <p className="modified">{convertDate(doc.client_modified)}</p>
+        <div className="dropDownCtn">
+          <button onClick={showDropDown} id={doc.id}>
+            <MdMenu />
+          </button>
+          <div className={dropdownClass}>
+            <Link
+              to="/"
+              className="deleteLink"
+              onClick={() => {
+                activateModal(doc.name, doc.id);
+              }}
+            >
+              Delete
+            </Link>
+          </div>
+        </div>
+      </li>
+    );
+  }
 };
 
 export default FileList;
