21c7336764bc73bf38d3f2c94f9ae3f4d8c963f3
adrienn timko <adrienn.timko@gmail.com>
2020-03-12 13:17:34 +0100

    changed convertBytes.test.js

--- src/FileList/convertBytes.test.js
+++ src/FileList/convertBytes.test.js
@@ -1,10 +1,10 @@
 import { convertBytes } from './convertBytes';
 
 describe('convertBytes', () => {
-    it('convertBytes should return KB if bytes', () => {
-      const inputSize = 7149;
-      const convertedSize = '7 KB';
+  it('convertBytes should return KB if bytes', () => {
+    const inputSize = 7149;
+    const convertedSize = '7.0 KB';
 
-      expect(convertBytes(inputSize)).toBe(convertedSize);
-    }) 
+    expect(convertBytes(inputSize)).toBe(convertedSize);
+  })
 })
